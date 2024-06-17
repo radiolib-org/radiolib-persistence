d02b9ae7884fc5d0efc58fcb6011068c7ea4110e
StevenCellist <47155822+StevenCellist@users.noreply.github.com>
2024-06-17 22:23:11 +0200

    [ESP32] Update to RadioLib 6.6.0

--- examples/LoRaWAN_ESP32/LoRaWAN_ESP32.ino
+++ examples/LoRaWAN_ESP32/LoRaWAN_ESP32.ino
@@ -17,6 +17,9 @@ implications for flash wearing and complications with which parts of the
 session may have changed after an uplink. So it is assumed that the device
 is going in to deep-sleep, as below, between normal uplinks.
 
+Once you understand what happens, feel free to delete the comments and 
+Serial.prints - we promise the final result isn't that many lines.
+
 */
 
 #if !defined(ESP32)
@@ -34,7 +37,7 @@ Preferences store;
 
 // utilities & vars to support ESP32 deep-sleep. The RTC_DATA_ATTR attribute
 // puts these in to the RTC memory which is preserved during deep-sleep
-RTC_DATA_ATTR uint16_t bootCount = 1;
+RTC_DATA_ATTR uint16_t bootCount = 0;
 RTC_DATA_ATTR uint16_t bootCountSinceUnsuccessfulJoin = 0;
 RTC_DATA_ATTR uint8_t LWsession[RADIOLIB_LORAWAN_SESSION_BUF_SIZE];
 
@@ -51,7 +54,7 @@ void print_wakeup_reason() {
   }
 
   Serial.print(F("Boot count: "));
-  Serial.println(bootCount++);
+  Serial.println(++bootCount);      // increment before printing
 }
 
 // put device in to lowest power deep-sleep mode
@@ -69,54 +72,61 @@ void gotoSleep(uint32_t seconds) {
   ESP.restart();
 }
 
+int16_t lwActivate() {
+  int16_t state = RADIOLIB_ERR_UNKNOWN;
 
-
-// setup & execute all device functions ...
-void setup() {
-  Serial.begin(115200);
-  while (!Serial);  							// wait for serial to be initalised
-  delay(2000);  									// give time to switch to the serial monitor
-  Serial.println(F("\nSetup"));
-  print_wakeup_reason();
-
-  int16_t state = 0;  						// return value for calls to RadioLib
-
-  // setup the radio based on the pinmap (connections) in config.h
-  Serial.println(F("Initalise the radio"));
-  state = radio.begin();
-  debug(state != RADIOLIB_ERR_NONE, F("Initalise radio failed"), state, true);
+  // setup the OTAA session information
+  node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
 
   Serial.println(F("Recalling LoRaWAN nonces & session"));
   // ##### setup the flash storage
   store.begin("radiolib");
-  // ##### if we have previously saved nonces, restore them
+  // ##### if we have previously saved nonces, restore them and try to restore session as well
   if (store.isKey("nonces")) {
     uint8_t buffer[RADIOLIB_LORAWAN_NONCES_BUF_SIZE];										// create somewhere to store nonces
-    store.getBytes("nonces", buffer, RADIOLIB_LORAWAN_NONCES_BUF_SIZE);	// get them to the store
+    store.getBytes("nonces", buffer, RADIOLIB_LORAWAN_NONCES_BUF_SIZE);	// get them from the store
     state = node.setBufferNonces(buffer); 															// send them to LoRaWAN
     debug(state != RADIOLIB_ERR_NONE, F("Restoring nonces buffer failed"), state, false);
+
+    // recall session from RTC deep-sleep preserved variable
+    state = node.setBufferSession(LWsession); // send them to LoRaWAN stack
+
+    // if we have booted more than once we should have a session to restore, so report any failure
+    // otherwise no point saying there's been a failure when it was bound to fail with an empty LWsession var.
+    debug((state != RADIOLIB_ERR_NONE) && (bootCount > 1), F("Restoring session buffer failed"), state, false);
+
+    // if Nonces and Session restored successfully, activation is just a formality
+    // moreover, Nonces didn't change so no need to re-save them
+    if (state == RADIOLIB_ERR_NONE) {
+      Serial.println(F("Succesfully restored session - now activating"));
+      state = node.activateOTAA();
+      debug((state != RADIOLIB_LORAWAN_SESSION_RESTORED), F("Failed to activate restored session"), state, true);
+
+      // ##### close the store before returning
+      store.end();
+      return(state);
+    }
+
+  } else {  // store has no key "nonces"
+    Serial.println(F("No Nonces saved - starting fresh."));
   }
 
-  // recall session from RTC deep-sleep preserved variable
-  state = node.setBufferSession(LWsession); // send them to LoRaWAN stack
-  // if we have booted at least once we should have a session to restore, so report any failure
-  // otherwise no point saying there's been a failure when it was bound to fail with an empty 
-  // LWsession var. At this point, bootCount has already been incremented, hence the > 2
-  debug((state != RADIOLIB_ERR_NONE) && (bootCount > 2), F("Restoring session buffer failed"), state, false);
-
-  // process the restored session or failing that, create a new one & 
-  // return flag to indicate a fresh join is required
-  Serial.println(F("Setup LoRaWAN session"));
-  state = node.beginOTAA(joinEUI, devEUI, nwkKey, appKey, false);
-  // see comment above, no need to report a failure that is bound to occur on first boot
-  debug((state != RADIOLIB_ERR_NONE) && (bootCount > 2), F("Restore session failed"), state, false);
-
-  // loop until successful join
-  while (state != RADIOLIB_ERR_NONE) {
+  // if we got here, there was no session to restore, so start trying to join
+  state = RADIOLIB_ERR_NETWORK_NOT_JOINED;
+  while (state != RADIOLIB_LORAWAN_NEW_SESSION) {
     Serial.println(F("Join ('login') to the LoRaWAN Network"));
-    state = node.beginOTAA(joinEUI, devEUI, nwkKey, appKey, true);
+    state = node.activateOTAA();
 
-    if (state < RADIOLIB_ERR_NONE) {
+    // ##### save the join counters (nonces) to permanent store
+    Serial.println(F("Saving nonces to flash"));
+    uint8_t buffer[RADIOLIB_LORAWAN_NONCES_BUF_SIZE];           // create somewhere to store nonces
+    uint8_t *persist = node.getBufferNonces();                  // get pointer to nonces
+    memcpy(buffer, persist, RADIOLIB_LORAWAN_NONCES_BUF_SIZE);  // copy in to buffer
+    store.putBytes("nonces", buffer, RADIOLIB_LORAWAN_NONCES_BUF_SIZE); // send them to the store
+    
+    // we'll save the session after an uplink
+
+    if (state != RADIOLIB_LORAWAN_NEW_SESSION) {
       Serial.print(F("Join failed: "));
       Serial.println(state);
 
@@ -133,49 +143,60 @@ void setup() {
 
       gotoSleep(sleepForSeconds);
 
-    } else {  // join was successful
-        Serial.println(F("Joined"));
-
-        // ##### save the join counters (nonces) to permanent store
-        Serial.println(F("Saving nonces to flash"));
-        uint8_t buffer[RADIOLIB_LORAWAN_NONCES_BUF_SIZE];           // create somewhere to store nonces
-        uint8_t *persist = node.getBufferNonces();                  // get pointer to nonces
-        memcpy(buffer, persist, RADIOLIB_LORAWAN_NONCES_BUF_SIZE);  // copy in to buffer
-        store.putBytes("nonces", buffer, RADIOLIB_LORAWAN_NONCES_BUF_SIZE); // send them to the store
-
-        // we'll save the session after the uplink
-
-        // reset the failed join count
-        bootCountSinceUnsuccessfulJoin = 0;
-
-        delay(1000);  // hold off off hitting the airwaves again too soon - an issue in the US
-
-    } // if beginOTAA state
+    } // if activateOTAA state
   } // while join
 
+  Serial.println(F("Joined"));
+
+  // reset the failed join count
+  bootCountSinceUnsuccessfulJoin = 0;
+
+  delay(1000);  // hold off off hitting the airwaves again too soon - an issue in the US
+  
   // ##### close the store
   store.end();  
-  
+  return(state);
+}
+
+// setup & execute all device functions ...
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);  							// wait for serial to be initalised
+  delay(2000);  									// give time to switch to the serial monitor
+  Serial.println(F("\nSetup"));
+  print_wakeup_reason();
+
+  int16_t state = 0;  						// return value for calls to RadioLib
+
+  // setup the radio based on the pinmap (connections) in config.h
+  Serial.println(F("Initalise the radio"));
+  state = radio.begin();
+  debug(state != RADIOLIB_ERR_NONE, F("Initalise radio failed"), state, true);
+
+  // activate node by restoring session or otherwise joining the network
+  state = lwActivate();
+  // state is one of RADIOLIB_LORAWAN_NEW_SESSION or RADIOLIB_LORAWAN_SESSION_RESTORED
 
   // ----- and now for the main event -----
   Serial.println(F("Sending uplink"));
 
-  // read some inputs
-  uint8_t Digital2 = digitalRead(2);
-  uint16_t Analog1 = analogRead(A1);
+  // this is the place to gather the sensor inputs
+  // instead of reading any real sensor, we just generate some random numbers as example
+  uint8_t value1 = radio.random(100);
+  uint16_t value2 = radio.random(2000);
 
   // build payload byte array
   uint8_t uplinkPayload[3];
-  uplinkPayload[0] = Digital2;
-  uplinkPayload[1] = highByte(Analog1);   // see notes for high/lowByte functions
-  uplinkPayload[2] = lowByte(Analog1);
+  uplinkPayload[0] = value1;
+  uplinkPayload[1] = highByte(value2);   // See notes for high/lowByte functions
+  uplinkPayload[2] = lowByte(value2);
   
   // perform an uplink
   state = node.sendReceive(uplinkPayload, sizeof(uplinkPayload));    
   debug((state != RADIOLIB_LORAWAN_NO_DOWNLINK) && (state != RADIOLIB_ERR_NONE), F("Error in sendReceive"), state, false);
 
   Serial.print(F("FcntUp: "));
-  Serial.println(node.getFcntUp());
+  Serial.println(node.getFCntUp());
 
   // now save session to RTC memory
   uint8_t *persist = node.getBufferSession();
